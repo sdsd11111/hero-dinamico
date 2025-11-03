60ac54e2b582308b59c8db38588a1fcd6906dbac
Tu Nombre <tu-email@ejemplo.com>
2025-11-03 18:31:52 -0500

    Configuración para Vercel

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['localhost'],
   },
+  trailingSlash: true,
+  basePath: '',
+  assetPrefix: './',
 };
 
 module.exports = nextConfig;
